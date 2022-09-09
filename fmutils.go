560cbe064c3c5f9d1eea972692516ec9d7f1c439
William Findlay <will@isovalent.com>
2022-09-09 16:43:07 -0400

    WIP
    Signed-off-by: William Findlay <will@isovalent.com>

--- fmutils.go
+++ fmutils.go
@@ -150,3 +150,182 @@ func (mask NestedMask) Prune(msg proto.Message) {
 		return true
 	})
 }
+
+// WildcardFilter keeps the msg fields that are listed in the paths and clears all the rest.
+// Supports wildcard keys in pathnames, specified by an asterisk, "*".
+//
+// This is a handy wrapper for WildcardNestedMask.Filter method.
+// If the same paths are used to process multiple proto messages use WildcardNestedMask.Filter method directly.
+func WildcardFilter(msg proto.Message, paths []string) {
+	WildcardNestedMaskFromPaths(paths).Filter(msg)
+}
+
+// WildcardPrune clears all the fields listed in paths from the given msg.
+// Supports wildcard keys in pathnames, specified by an asterisk, "*".
+//
+// This is a handy wrapper for WildcardNestedMask.Prune method.
+// If the same paths are used to process multiple proto messages use WildcardNestedMask.Filter method directly.
+func WildcardPrune(msg proto.Message, paths []string) {
+	WildcardNestedMaskFromPaths(paths).Prune(msg)
+}
+
+// WildcardNestedMask represents a field mask with support for wildcards as a recursive map.
+type WildcardNestedMask map[string]WildcardNestedMask
+
+// WildcardNestedMaskFromPaths creates an instance of WildcardNestedMask for the given paths.
+func WildcardNestedMaskFromPaths(paths []string) WildcardNestedMask {
+	mask := make(WildcardNestedMask)
+	for _, path := range paths {
+		curr := mask
+		var letters []rune
+		for _, letter := range path {
+			if letter == '.' {
+				if len(letters) == 0 {
+					continue
+				}
+
+				key := string(letters)
+				c, ok := curr[key]
+				if !ok {
+					c = make(WildcardNestedMask)
+					curr[key] = c
+				}
+				curr = c
+				letters = nil
+				continue
+			}
+			letters = append(letters, letter)
+		}
+		if len(letters) != 0 {
+			key := string(letters)
+			if _, ok := curr[key]; !ok {
+				curr[key] = make(WildcardNestedMask)
+			}
+		}
+	}
+
+	return mask
+}
+
+func (mask WildcardNestedMask) filterField(
+	rft protoreflect.Message,
+	fd protoreflect.FieldDescriptor,
+	wildcard, pruning bool,
+) bool {
+	var key string
+	if wildcard {
+		key = "*"
+	} else {
+		key = string(fd.Name())
+	}
+
+	m, ok := mask[key]
+	if ok {
+		if len(m) == 0 {
+			return true
+		}
+
+		if fd.IsMap() {
+			xmap := rft.Get(fd).Map()
+			xmap.Range(func(mk protoreflect.MapKey, mv protoreflect.Value) bool {
+				if mask.filterMapKey(xmap, mk, mv, false) {
+					if pruning {
+						xmap.Clear(mk)
+					}
+					return true
+				}
+				if mask.filterMapKey(xmap, mk, mv, true) {
+					if pruning {
+						xmap.Clear(mk)
+					}
+					return true
+				}
+				if !pruning {
+					xmap.Clear(mk)
+				}
+				return false
+			})
+		} else if fd.IsList() {
+			list := rft.Get(fd).List()
+			for i := 0; i < list.Len(); i++ {
+				m.Filter(list.Get(i).Message().Interface())
+			}
+		} else if fd.Kind() == protoreflect.MessageKind {
+			m.Filter(rft.Get(fd).Message().Interface())
+		}
+	}
+	return true
+}
+
+func (mask WildcardNestedMask) filterMapKey(
+	xmap protoreflect.Map,
+	mk protoreflect.MapKey,
+	mv protoreflect.Value,
+	wildcard bool,
+) bool {
+	var key string
+	if wildcard {
+		key = "*"
+	} else {
+		key = mk.String()
+	}
+
+	m, ok := mask[key]
+	if ok {
+		if i, ok := mv.Interface().(protoreflect.Message); ok && len(m) > 0 {
+			m.Filter(i.Interface())
+		}
+	}
+
+	return true
+}
+
+// Filter keeps the msg fields that are listed in the paths and clears all the rest.
+// Supports wildcard keys in pathnames, specified by an asterisk, "*".
+//
+// If the mask is empty then all the fields are kept.
+// Paths are assumed to be valid and normalized otherwise the function may panic.
+// See google.golang.org/protobuf/types/known/fieldmaskpb for details.
+func (mask WildcardNestedMask) Filter(msg proto.Message) {
+	if len(mask) == 0 {
+		return
+	}
+
+	rft := msg.ProtoReflect()
+	rft.Range(func(fd protoreflect.FieldDescriptor, _ protoreflect.Value) bool {
+		if mask.filterField(rft, fd, false, false) {
+			return true
+		}
+		if mask.filterField(rft, fd, true, false) {
+			return true
+		}
+		rft.Clear(fd)
+		return false
+	})
+}
+
+// Prune clears all the fields listed in paths from the given msg.
+// Supports wildcard keys in pathnames, specified by an asterisk, "*".
+//
+// All other fields are kept untouched. If the mask is empty no fields are cleared.
+// This operation is the opposite of WildcardNestedMask.Filter.
+// Paths are assumed to be valid and normalized otherwise the function may panic.
+// See google.golang.org/protobuf/types/known/fieldmaskpb for details.
+func (mask WildcardNestedMask) Prune(msg proto.Message) {
+	if len(mask) == 0 {
+		return
+	}
+
+	rft := msg.ProtoReflect()
+	rft.Range(func(fd protoreflect.FieldDescriptor, _ protoreflect.Value) bool {
+		if mask.filterField(rft, fd, false, true) {
+			rft.Clear(fd)
+			return true
+		}
+		if mask.filterField(rft, fd, true, true) {
+			rft.Clear(fd)
+			return true
+		}
+		return false
+	})
+}
--- fmutils_test.go
+++ fmutils_test.go
@@ -1,6 +1,7 @@
 package fmutils
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -771,5 +772,552 @@ func TestPrune(t *testing.T) {
 func BenchmarkNestedMaskFromPaths(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		NestedMaskFromPaths([]string{"aaa.bbb.c.d.e.f", "aa.b.cc.ddddddd", "e", "f", "g.h.i.j.k"})
+	}
+}
+
+func Test_WildcardWildcardNestedMaskFromPaths(t *testing.T) {
+	type args struct {
+		paths []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want WildcardNestedMask
+	}{
+		{
+			name: "no nested fields",
+			args: args{paths: []string{"a", "b", "c"}},
+			want: WildcardNestedMask{"a": WildcardNestedMask{}, "b": WildcardNestedMask{}, "c": WildcardNestedMask{}},
+		},
+		{
+			name: "with nested fields",
+			args: args{paths: []string{"aaa.*.c", "dd.e", "f", "*.*.*.g"}},
+			want: WildcardNestedMask{
+				"aaa": WildcardNestedMask{"*": WildcardNestedMask{"c": WildcardNestedMask{}}},
+				"dd":  WildcardNestedMask{"e": WildcardNestedMask{}},
+				"f":   WildcardNestedMask{},
+				"*":   WildcardNestedMask{"*": WildcardNestedMask{"*": WildcardNestedMask{"g": WildcardNestedMask{}}}}},
+		},
+		{
+			name: "single field",
+			args: args{paths: []string{"*"}},
+			want: WildcardNestedMask{"*": WildcardNestedMask{}},
+		},
+		{
+			name: "empty fields",
+			args: args{paths: []string{}},
+			want: WildcardNestedMask{},
+		},
+		{
+			name: "invalid input",
+			args: args{paths: []string{".", "..", "..."}},
+			want: WildcardNestedMask{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := WildcardNestedMaskFromPaths(tt.args.paths); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("WildcardNestedMaskFromPaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWildcardFilter(t *testing.T) {
+	tests := []struct {
+		name  string
+		paths []string
+		msg   proto.Message
+		want  proto.Message
+	}{
+		{
+			name:  "empty mask keeps all the fields",
+			paths: []string{},
+			msg: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+				Photo: &testproto.Photo{
+					PhotoId: 2,
+					Path:    "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width:  100,
+						Height: 120,
+					},
+				},
+				LoginTimestamps: []int64{1, 2},
+			},
+			want: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+				Photo: &testproto.Photo{
+					PhotoId: 2,
+					Path:    "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width:  100,
+						Height: 120,
+					},
+				},
+				LoginTimestamps: []int64{1, 2},
+			},
+		},
+		{
+			name:  "mask with all root fields keeps all root fields",
+			paths: []string{"user", "photo"},
+			msg: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+				Photo: &testproto.Photo{
+					PhotoId: 2,
+					Path:    "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width:  100,
+						Height: 120,
+					},
+				},
+			},
+			want: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+				Photo: &testproto.Photo{
+					PhotoId: 2,
+					Path:    "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width:  100,
+						Height: 120,
+					},
+				},
+			},
+		},
+		{
+			name:  "mask with single root field keeps that field only",
+			paths: []string{"user"},
+			msg: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+				Photo: &testproto.Photo{
+					PhotoId: 2,
+					Path:    "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width:  100,
+						Height: 120,
+					},
+				},
+			},
+			want: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+			},
+		},
+		{
+			name:  "mask with nested fields keeps the listed fields only",
+			paths: []string{"user.name", "photo.path", "photo.dimensions.width"},
+			msg: &testproto.Profile{
+				User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				},
+				Photo: &testproto.Photo{
+					PhotoId: 2,
+					Path:    "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width:  100,
+						Height: 120,
+					},
+				},
+			},
+			want: &testproto.Profile{
+				User: &testproto.User{
+					Name: "user name",
+				},
+				Photo: &testproto.Photo{
+					Path: "photo path",
+					Dimensions: &testproto.Dimensions{
+						Width: 100,
+					},
+				},
+			},
+		},
+		{
+			name:  "mask with oneof field keeps the entire field",
+			paths: []string{"user"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_User{User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				}},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_User{User: &testproto.User{
+					UserId: 1,
+					Name:   "user name",
+				}},
+			},
+		},
+		{
+			name:  "mask with nested oneof fields keeps listed fields only",
+			paths: []string{"profile.photo.dimensions", "profile.user.user_id", "profile.login_timestamps"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Profile{Profile: &testproto.Profile{
+					User: &testproto.User{
+						UserId: 1,
+						Name:   "user name",
+					},
+					Photo: &testproto.Photo{
+						PhotoId: 1,
+						Path:    "photo path",
+						Dimensions: &testproto.Dimensions{
+							Width:  100,
+							Height: 120,
+						},
+					},
+					LoginTimestamps: []int64{1, 2, 3},
+				}},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Profile{Profile: &testproto.Profile{
+					User: &testproto.User{
+						UserId: 1,
+					},
+					Photo: &testproto.Photo{
+						Dimensions: &testproto.Dimensions{
+							Width:  100,
+							Height: 120,
+						},
+					},
+					LoginTimestamps: []int64{1, 2, 3},
+				}},
+			},
+		},
+		{
+			name:  "mask with Any field in oneof field keeps the entire Any field",
+			paths: []string{"details"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Details{Details: createAny(&testproto.Result{
+					Data:      []byte("bytes"),
+					NextToken: 1,
+				})},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Details{Details: createAny(&testproto.Result{
+					Data:      []byte("bytes"),
+					NextToken: 1,
+				})},
+			},
+		},
+		{
+			name:  "mask with repeated nested fields keeps the listed fields",
+			paths: []string{"profile.gallery.photo_id", "profile.gallery.dimensions.height"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Photo: &testproto.Photo{
+							PhotoId: 4,
+							Path:    "photo path",
+						},
+						Gallery: []*testproto.Photo{
+							{
+								PhotoId: 1,
+								Path:    "path 1",
+								Dimensions: &testproto.Dimensions{
+									Width:  100,
+									Height: 200,
+								},
+							},
+							{
+								PhotoId: 2,
+								Path:    "path 2",
+								Dimensions: &testproto.Dimensions{
+									Width:  300,
+									Height: 400,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Gallery: []*testproto.Photo{
+							{
+								PhotoId: 1,
+								Dimensions: &testproto.Dimensions{
+									Height: 200,
+								},
+							},
+							{
+								PhotoId: 2,
+								Dimensions: &testproto.Dimensions{
+									Height: 400,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "mask with repeated field keeps the listed field only",
+			paths: []string{"profile.gallery"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Photo: &testproto.Photo{
+							PhotoId: 4,
+							Path:    "photo path",
+						},
+						Gallery: []*testproto.Photo{
+							{
+								PhotoId: 1,
+								Path:    "path 1",
+								Dimensions: &testproto.Dimensions{
+									Width:  100,
+									Height: 200,
+								},
+							},
+							{
+								PhotoId: 2,
+								Path:    "path 2",
+								Dimensions: &testproto.Dimensions{
+									Width:  300,
+									Height: 400,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Gallery: []*testproto.Photo{
+							{
+								PhotoId: 1,
+								Path:    "path 1",
+								Dimensions: &testproto.Dimensions{
+									Width:  100,
+									Height: 200,
+								},
+							},
+							{
+								PhotoId: 2,
+								Path:    "path 2",
+								Dimensions: &testproto.Dimensions{
+									Width:  300,
+									Height: 400,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "mask with map field keeps the listed field only",
+			paths: []string{"profile.attributes.a1", "profile.attributes.a2.tags.t2", "profile.attributes.aNonExistant"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Attributes: map[string]*testproto.Attribute{
+							"a1": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a2": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a3": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Attributes: map[string]*testproto.Attribute{
+							"a1": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a2": {
+								Tags: map[string]string{
+									"t2": "2",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "mask with wildcard value",
+			paths: []string{"profile.attributes.*", "profile.attributes.a2.tags.t2", "profile.attributes.aNonExistant"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Attributes: map[string]*testproto.Attribute{
+							"a1": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a2": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a3": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Attributes: map[string]*testproto.Attribute{
+							"a1": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a2": {
+								Tags: map[string]string{
+									"t2": "2",
+								},
+							},
+							"a3": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "mask with lots of wildcard values",
+			paths: []string{"profile.attributes.*.tags.t1", "profile.*.*.tags.t3"},
+			msg: &testproto.Event{
+				EventId: 1,
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Attributes: map[string]*testproto.Attribute{
+							"a1": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a2": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+							"a3": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t2": "2",
+									"t3": "3",
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &testproto.Event{
+				Changed: &testproto.Event_Profile{
+					Profile: &testproto.Profile{
+						Attributes: map[string]*testproto.Attribute{
+							"a1": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t3": "3",
+								},
+							},
+							"a2": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t3": "3",
+								},
+							},
+							"a3": {
+								Tags: map[string]string{
+									"t1": "1",
+									"t3": "3",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println("new test", tt.name, "starts here -----------------")
+			WildcardFilter(tt.msg, tt.paths)
+			if !proto.Equal(tt.msg, tt.want) {
+				t.Errorf("msg %v, want %v", tt.msg, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkWilcardNestedMaskFromPaths(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		WildcardNestedMaskFromPaths([]string{"aaa.bbb.c.d.e.f", "aa.b.cc.ddddddd", "e", "f", "g.h.i.j.k", "*.*.*.*.a.*.*.bbbbb.*.cccccc"})
 	}
 }
